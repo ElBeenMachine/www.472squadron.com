bd4ab70620a5a5416fc53fc1ce91aa6976a2c3d2
Ollie Beenham <beenhamow@outlook.com>
2023-05-14 01:42:19 +0100

    Page now has navbar, footer and loader

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Layout from "@/components/Global/Layout";
+
+export default function Home() {
+    return (
+        <Layout pageTitle={"472 SQUADRON | Home"} />
+    )
+}
---
+++ styles/colours.js
@@ -0,0 +1,6 @@
+const colourScheme = {
+    primaryAccent: "#2987b3",
+    primaryHover: "#206c8f"
+}
+
+export default colourScheme;
