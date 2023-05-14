c012f90dffcfe0cf4137b220d0631d190f655f23
Ollie Beenham <beenhamow@outlook.com>
2023-05-14 13:26:11 +0100

    Added about section and testimonials
    Added the following:
    - About section
    - Updated header and footer links
    - Testimonials (2 still need to be finished)

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Layout from "@/components/Global/Layout";
+import About from "@/components/Homepage/About";
 import Hero from "@/components/Homepage/Hero";
+import Testimonials from "@/components/Homepage/Testimonials";
+import { Container } from "@chakra-ui/react";
 
 export default function Home() {
     return (
-        <Layout pageTitle={"472 SQUADRON | Home"}>
+        <Layout pageTitle={"472 Squadron | Home"}>
             <Hero />
+            <Container id="main" maxW={'5xl'}>
+                <About />
+                <Testimonials />
+            </Container>
         </Layout>
     )
 }
